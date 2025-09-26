b49f80c928393b661e12a1ddcbce1d628461a52e
Manik MT <manikmt01@gmail.com>
2025-09-26 12:10:59 +0600

    new landing page v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,37 +16,36 @@ module.exports = {
           4: '#272727',
           5: '#222222',
           6: '#1b1b1b',
-          7:'#3a3a3a',
+          7: '#3a3a3a',
         },
         white: {
           1: '#ffffff',
           2: '#D4D4DA',
         },
+        whitePure: '#FFFFFF',
         purple: {
           1: '#7B53E7',
-          2:'#8159EC',
+          2: '#8159EC',
         },
         blue: {
           1: '#0052CC',
         },
       },
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        avenir: ["Avenir Next Cyr Medium", "sans-serif"],
+        inter: ['Inter', 'sans-serif'],
+        avenir: ['Avenir Next Cyr Medium', 'sans-serif'],
       },
       backgroundImage: {
         gettingPrintumo: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1170' height='350' viewBox='0 0 1170 350' fill='none'%3E%3Cpath opacity='0.15' d='M0 0H1170V350H0V0Z' fill='url(%23paint0_linear_658_6023)'/%3E%3Cdefs%3E%3ClinearGradient id='paint0_linear_658_6023' x1='-20.3125' y1='34.528' x2='978.73' y2='744.957' gradientUnits='userSpaceOnUse'%3E%3Cstop stop-color='%235799EB'/%3E%3Cstop offset='1' stop-color='%239F74FB'/%3E%3C/linearGradient%3E%3C/defs%3E%3C/svg%3E")`,
         headerBiddingBg: 'url(./images/headerBiddingBg.png)',
-        
-        
       },
       screens: {
         '2xl': '1440px',
-        '3xl':'1660px'
+        '3xl': '1660px',
       },
       boxShadow: {
         headerboxshadow: '4px 10px 20px 0px rgba(199, 199, 199, 0.3)',
-        publishShadow:'0px 4px 62px -3px #9EA4B426'
+        publishShadow: '0px 4px 62px -3px #9EA4B426',
       },
     },
   },
